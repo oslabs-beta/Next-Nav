dafdb0c921e29ab77928d04bed40d99e4c9d3e6b
Brian Henkel <henkel.87@gmail.com>
2023-08-21 20:12:15 -0700

    add web dev test environment refactor show all nodes in  layout view
    Co-authored-by: Nathan Peel <nathanpeel@users.noreply.github.com>
    Co-authored-by: Jordan Q. <jequerubin@users.noreply.github.com>

--- webview-react-app/src/App.tsx
+++ webview-react-app/src/App.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import TreeContainer from './components/TreeContainer';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Next.Nav</h1>
-      <button>Import Next Project Root Folder</button>
-      <TreeContainer />
-    </div>
-  );
+  return <TreeContainer />;
 };
 
 export default App;
